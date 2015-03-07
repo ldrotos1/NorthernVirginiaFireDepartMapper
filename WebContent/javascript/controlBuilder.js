9b0cc6c6112345162582d2cdb277bf8ddbdbf66b
Louis Drotos <louis.drotos@gmail.com>
2015-03-07 14:53:00 -0500

    Add code for computing and setting search button height

--- WebContent/javascript/controlBuilder.js
+++ WebContent/javascript/controlBuilder.js
@@ -6,7 +6,8 @@
  * Jquery UI controls.
  */
 
-
+// This function initializes map pane controls once the
+// document is ready.
 $(function() {
 	
 	// Initializes the controls on the control pane 
@@ -19,11 +20,16 @@ $(function() {
 	$( "#deptCombo" ).selectmenu();
 	$( "#stationCombo" ).selectmenu();
 	
+	// Positions and sizes the search button
 	alignSearchBtn();
+	setSearchBtnHeight();
 });
 
+// This function aligns the top of the search button with the
+// top of the select boxes
 function alignSearchBtn() {
 	
+	// Declares variables
 	var objCmbLabel,
 	objSearchBtn,
 	strLabelMargin,
@@ -33,26 +39,52 @@ function alignSearchBtn() {
 	dblLabelHeight,
 	dblCmbHeight;
 	
-	
-	
+	// Gets the label height and margin
 	objCmbLabel = $( ".labelDiv" );
-	strLabelMargin = objCmbLabel.css("margin-bottom")
+	strLabelMargin = objCmbLabel.css("margin-bottom");
 	strLabelHeight = objCmbLabel.css("height");
 	
+	// Converts the label height and margin to double
 	dblLabelMargin = parseFloat(strLabelMargin.substring(0, strLabelMargin.length - 2));
 	dblLabelHeight = parseFloat(strLabelHeight.substring(0, strLabelHeight.length - 2));
 	
-	strCmbHeight = $( ".ui-selectmenu-button" ).css("height"); 
-	dblCmbHeight = parseFloat(strCmbHeight.substring(0, strCmbHeight.length - 2));
-	
+	// Sets the top margin of the search button
 	objSearchBtn = $( "#searchBtnDiv" );
 	objSearchBtn.css("margin-top", dblLabelMargin + dblLabelHeight);
-	objSearchBtn.css("")
+}
+
+// This function sets the size of the search button to be the
+// same as the height of the select boxes
+function setSearchBtnHeight() {
 	
-	//$( "#searchBtnDiv" ).css("margin-top", dblLabelMargin + dblLabelHeight);
-	//position = $( "#stationDiv" ).offset().top;
-	//$( "#searchBtn" ).css("position", "fixed");
-	//$( "#searchBtn" ).css("top", position);
+	var objSearchBtn, 
+	strSelectHeight,
+	strButtonHeight,
+	strButtonPadding,
+	strButtonBorder,
+	dblSelectHeight,
+	dblButtonHeight,
+	dblButtonPadding,
+	dblButtonBorder,
+	dblNewBtnPadding;
+	
+	// Gets measurements
+	objSearchBtn = $( "#searchBtn" );
+	strButtonBorder = objSearchBtn.css("border"); 
+	strButtonPadding = objSearchBtn.css("padding-top");
+	strButtonHeight = objSearchBtn.css("height");
+	strSelectHeight = $( "#unitCombo-button" ).css("height"); 
+
+	// Converts measurements to double
+	dblSelectHeight = parseFloat(strSelectHeight.substring(0, strSelectHeight.length - 2));
+	dblButtonHeight = parseFloat(strButtonHeight.substring(0, strButtonHeight.length - 2));
+	dblButtonPadding = parseFloat(strButtonPadding.substring(0, strButtonPadding.length - 2));
+	dblButtonBorder = parseFloat(strButtonBorder.substring(0, strButtonBorder.length - 2));
+	
+	// Increases the button padding to adjust the height 
+	dblNewBtnPadding = (( dblSelectHeight - dblButtonHeight ) / 2 ) + dblButtonPadding;
+	objSearchBtn.css("padding-top", dblNewBtnPadding);
+	objSearchBtn.css("padding-bottom", dblNewBtnPadding);
 }
 
 
