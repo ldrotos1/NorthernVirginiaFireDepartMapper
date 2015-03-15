544fdbfe6843c8d72fff2545f846f03b3f76e4e9
Louis Drotos <louis.drotos@gmail.com>
2015-03-15 12:13:20 -0400

    Added code for Station entity class

---
+++ src/com/fire/model/Station.java
@@ -0,0 +1,231 @@
+package com.fire.model;
+
+import java.nio.file.Path;
+
+import org.postgis.Point;
+
+/**
+ * This entity class provides a representation of a fire station
+ * 
+ * @author Louis Drotos - 15 March 2015
+ *
+ */
+public class Station {
+	
+	private Point location;
+	private String stationId;
+	private String stationNumber;
+	private String stationName;
+	private String department;
+	private String address;
+	private String city;
+	private String state;
+	private String zipCode;
+	private Path imageUrl;
+	
+	/**
+	 * Constructor
+	 */
+	public Station() {
+		super();
+	}
+	
+	/**
+	 * Getter - Station Location
+	 * 
+	 * @return The location of this station
+	 */
+	public Point getLocation() {
+		return location;
+	}
+
+	/**
+	 * Setter - Station Location
+	 * 
+	 * @param location The location of this station
+	 */
+	public void setLocation(Point location) {
+		this.location = location;
+	}
+	
+	/**
+	 * Getter - Station ID
+	 * 
+	 * @return The station ID of this station
+	 */
+	public String getStationId() {
+		return stationId;
+	}
+
+	/**
+	 * Setter - Station ID
+	 * 
+	 * @param stationId The station ID of this station
+	 */
+	public void setStationId(String stationId) {
+		this.stationId = stationId;
+	}
+	
+	/**
+	 * Getter - Station Number
+	 * 
+	 * @return This station's station number
+	 */
+	public String getStationNumber() {
+		return stationNumber;
+	}
+
+	/**
+	 * Setter - Station Number
+	 * 
+	 * @param stationNumber This station's station number
+	 */
+	public void setStationNumber(String stationNumber) {
+		this.stationNumber = stationNumber;
+	}
+
+	/**
+	 * Getter - Station Name
+	 * 
+	 * @return The name of this station
+	 */
+	public String getStationName() {
+		return stationName;
+	}
+
+	/**
+	 * Setter - Station Name
+	 * 
+	 * @param stationName The name of this station
+	 */
+	public void setStationName(String stationName) {
+		this.stationName = stationName;
+	}
+	
+	/**
+	 * Getter - Department
+	 * 
+	 * @return This station's department
+	 */
+	public String getDepartment() {
+		return department;
+	}
+
+	/**
+	 * Setter - Department
+	 * 
+	 * @param department This station's department
+	 */
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	/**
+	 * Getter - Address
+	 * 
+	 * @return This station's address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * Setter - Address
+	 * 
+	 * @param address This station's address
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * Getter - City
+	 * 
+	 * @return This station's city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * Setter - City
+	 * 
+	 * @param city This station's city
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * Getter - State
+	 * 
+	 * @return This station's state
+	 */
+	public String getState() {
+		return state;
+	}
+
+	/**
+	 * Setter - State
+	 * 
+	 * @param state This station's state
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	/**
+	 * Getter - Zip Code
+	 * 
+	 * @return This station's zip code
+	 */
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	/**
+	 * Setter - Zip Code
+	 * 
+	 * @param zipCode This station's zip code 
+	 */
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	/**
+	 * Getter - Image URL
+	 * 
+	 * @return The URL of this station's image
+	 */
+	public Path getImageUrl() {
+		return imageUrl;
+	}
+
+	/**
+	 * Setter - Image URL
+	 * 
+	 * @param imageUrl The URL of this station's image
+	 */
+	public void setImageUrl(Path imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+	/**
+	 * Returns a string representation of this object
+	 * 
+	 * @return The string representation
+	 */
+	@Override
+	public String toString() {
+		
+		StringBuilder builder = new StringBuilder();
+		builder.append("Station ID : " + this.getStationId());
+		builder.append("\nStation Number : " + this.getStationNumber());
+		builder.append("\nStation Name : " + this.getStationName());
+		builder.append("\nDepartment : " + this.department);
+		builder.append("\nAddress : " + this.address + " " + this.city + ", " + this.state + " " + this.zipCode);
+		builder.append("\nLat\\Long : " + String.valueOf(this.getLocation().y) + "\\" + String.valueOf(this.getLocation().x));
+		builder.append("\nStation Image : " + this.imageUrl.toString());
+		return builder.toString().trim();	
+	}
+}
