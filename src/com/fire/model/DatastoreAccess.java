a9fd46c4441879ac2031b7165ebd1990e280ca6f
Louis Drotos <louis.drotos@gmail.com>
2015-03-15 12:54:37 -0400

    Updated datastore class to include code for querying Station objects and apparatus

--- src/com/fire/model/DatastoreAccess.java
+++ src/com/fire/model/DatastoreAccess.java
@@ -27,10 +27,57 @@ import org.postgis.Point;
 public class DatastoreAccess {
 
 	/**
+	 * Returns a set containing an entity station object for each station in the database.
+	 * 
+	 * @param imageDirectory The directory containing the images of the stations.
+	 * @param conn The database connection.
+	 * @return The set of stations.
+	 * @throws SQLException
+	 */
+	public Set<Station> getAllStations(Path imageDirectory, Connection conn) throws SQLException {
+
+		// Declares objects
+		Set<Station> stations;
+		Station station;
+		ResultSet results;
+		String sql;
+		PGgeometry geom;
+		
+		// Runs the query
+		sql = "SELECT station_id, station_name, station_number, geom, department, address, city, state, zipcode FROM station";
+		results = queryDatabase(conn, sql);
+		
+		// Adds the query results to the set
+		stations = new HashSet<Station>();
+		while (results.next() == true) {
+			
+			// Builds the station info object
+			station = new Station();
+			geom = (PGgeometry)results.getObject("geom");
+			station.setLocation((Point)geom.getGeometry());
+			station.setStationId(results.getString("station_id"));
+			station.setStationNumber(results.getString("station_number"));
+			station.setStationName(results.getString("station_name"));
+			station.setDepartment(results.getString("department"));
+			station.setAddress(results.getString("address"));
+			station.setCity(results.getString("city"));
+			station.setState(results.getString("state"));
+			station.setZipCode(results.getString("zipcode"));
+			station.setImageUrl(imageDirectory.resolve(station.getStationId()));
+			
+			// Adds the station info object to the set
+			stations.add(station);
+		}
+		
+		// Returns the set
+		return stations;
+	}
+	
+	/**
 	 * Returns a set containing the basic station information for all stations
 	 * 
 	 * @param conn The database connection
-	 * @return	   The set of basic station info
+	 * @return The set of basic station info
 	 * @throws SQLException
 	 */
 	public Set<BasicStationInfo> getAllBasicStationInfo(Connection conn) throws SQLException {
@@ -69,7 +116,7 @@ public class DatastoreAccess {
 	 * Returns a set of all unique unit types found in the database.
 	 * 
 	 * @param conn The database connection
-	 * @return     The set of all units  
+	 * @return The set of all units  
 	 * @throws SQLException
 	 */
 	public List<String> getAllUnitTypes(Connection conn) throws SQLException {
@@ -98,7 +145,7 @@ public class DatastoreAccess {
 	 * Returns a set of all unique department names found in the database.
 	 * 
 	 * @param conn The database connection
-	 * @return     The set of all department names
+	 * @return The set of all department names
 	 * @throws SQLException
 	 */
 	public List<String> getAllDepartmentNames(Connection conn) throws SQLException {
@@ -127,7 +174,7 @@ public class DatastoreAccess {
 	 * Returns a set of all unique station names found in the database
 	 * 
 	 * @param conn The database connection
-	 * @return     The set of all station names
+	 * @return The set of all station names
 	 * @throws SQLException
 	 */
 	public List<String> getAllStationNames(Connection conn) throws SQLException {
@@ -158,7 +205,7 @@ public class DatastoreAccess {
 	 * 
 	 * @param department The department 
 	 * @param conn The database connection
-	 * @return		The set of station IDs
+	 * @return The set of station IDs
 	 * @throws SQLException 
 	 */
 	public Set<String> getStationIds(String department, Connection conn) throws SQLException {
@@ -188,8 +235,9 @@ public class DatastoreAccess {
 	 * will return null.  
 	 * 
 	 * @param stationID The ID of a station
+	 * @param imageDirectory The directory containing the images of the stations.
 	 * @param conn The database connection
-	 * @return		The station and apparatus information
+	 * @return The station and apparatus information
 	 * @throws SQLException 
 	 */
 	public FullStationInfo getFullStationInfo(String stationId, Path imageDirectory,  Connection conn) throws SQLException {
@@ -255,7 +303,7 @@ public class DatastoreAccess {
 	 * 
 	 * @param unitType The target apparatus type
 	 * @param conn The database connection
-	 * @return		The map collection
+	 * @return The map collection
 	 * @throws SQLException 
 	 */
 	public Map<String, String> getUnitCountByStation(String unitType, Connection conn) throws SQLException {
@@ -282,11 +330,46 @@ public class DatastoreAccess {
 	}
 	
 	/**
+	 * This method returns a list of apparatus assigned to a particular station
+	 * 
+	 * @param stationId The station ID of the station.
+	 * @param conn The database connection.
+	 * @return The list of apparatus assigned to the station.
+	 * @throws SQLException 
+	 */
+	public List<Apparatus> getApparatus(String stationId, Connection conn) throws SQLException {
+		
+		// Declare objects
+		Apparatus unit;
+		List<Apparatus> units;
+		ResultSet results;
+		String sql;
+		
+		// Runs the query
+		sql = "SELECT unit_designator, unit_type FROM apparatus WHERE station_id = \'" + stationId + "\'";
+		results = queryDatabase(conn, sql);
+		
+		// Cycles through the results and adds the apparatuses to the list
+		units = new LinkedList<Apparatus>();
+		while (results.next() == true) {
+			
+			unit = new Apparatus();
+			unit.setStationId(stationId);
+			unit.setUnitDesignator(results.getString("unit_designator"));
+			unit.setUnitType(results.getString("unit_type"));
+			units.add(unit);
+		}
+		
+		// Returns the list
+		return units;
+	}
+	
+	/**
 	 * This method executes a SQL query against the database. 
 	 * 
 	 * @param conn The connection to the database
 	 * @param sql The SQL query to execute
-	 * @return    The result set from the query  
+	 * @return The result set from the query  
 	 * @throws SQLException
 	 */
 	private synchronized ResultSet queryDatabase(Connection conn, String sql) throws SQLException {
